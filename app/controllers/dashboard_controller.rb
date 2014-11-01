bfe2202880d9ffe43425147998729a4dea085b1b
AndyNhi <nhi.atn@gmail.com>
2014-11-01 00:06:17 -0600

    twitter api, moved oauth to controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,12 @@
 class DashboardController < ApplicationController
 
   def index
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TW_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TW_CONSUMER_SECRET']
+      config.access_token        = ENV['TW_OAUTH_TOKEN']
+      config.access_token_secret = ENV['TW_OAUTH_TOKEN_SECRET']
+    end
   end
-
+  
 end
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+# $client = Twitter::REST::Client.new do |config|
+#   config.consumer_key        = ENV['TW_CONSUMER_KEY']
+#   config.consumer_secret     = ENV['TW_CONSUMER_SECRET']
+#   config.access_token        = ENV['TW_OAUTH_TOKEN']
+#   config.access_token_secret = ENV['TW_OAUTH_TOKEN_SECRET']
+# end
