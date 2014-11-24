1c71842ece4d96efc3f3e6418e00d727952ff6e1
AndyNhi <nhi.atn@gmail.com>
2014-11-23 23:22:25 -0700

    routes alignment

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :quotes, only: [:create, :destroy]
   end
 
-  get '/signout', to: 'authentication#destroy', as: :signout
- post '/signin',  to: 'authentication#create', as: :signin
+  get  '/signout', to: 'authentication#destroy', as: :signout
+  post '/signin',  to: 'authentication#create',  as: :signin
+
 end
