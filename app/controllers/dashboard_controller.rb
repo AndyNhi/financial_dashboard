f86116fd97550ffe07751acf2fb7f2bbe3eb1056
AndyNhi <nhi.atn@gmail.com>
2014-11-23 22:03:34 -0700

    condition statement in index of dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,17 @@
 class DashboardController < ApplicationController
 
   def index
+
+    if current_user.present?
+
     @portfolio_tweet_string = ""
     current_user.quotes.each do |hash|
       @portfolio_tweet_string += " OR $#{hash.ticker}"
     end
     @portfolio_tweet_string
+
+    end 
+
   end
 
 end
